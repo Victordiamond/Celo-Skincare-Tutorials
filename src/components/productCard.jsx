6dc067a1b46d5578603ad82c1149f3125402d921
Victoediamond <Noblediamond4@gmail.com>
2023-06-09 05:45:04 +0100

    main

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function ProductCard({
   orderProduct,
+  refundProduct, // New function for refunding the product
+  reviewProduct, // New function for reviewing the product
   id,
   brand,
   image,
@@ -18,12 +20,11 @@ export default function ProductCard({
       <div className="card-body">
         <h5 className="card-title d-flex">
           <div>
-            {" "}
             {brand} <br />
             <sup>{category}</sup>
           </div>
           <div className="price ms-auto">
-            {amount / 1000000000000000000} cUSD
+            {amount / 10 ** 18} cUSD
           </div>
         </h5>
         <ul className="nav flex-column">
@@ -36,6 +37,12 @@ export default function ProductCard({
         </ul>
         <button className="btn btn-primary" onClick={() => orderProduct(id)}>
           Order
+        </button>
+        <button className="btn btn-danger mt-2" onClick={() => refundProduct(id)}>
+          Refund
+        </button>
+        <button className="btn btn-secondary mt-2" onClick={() => reviewProduct(id)}>
+          Review
         </button>
       </div>
     </div>
